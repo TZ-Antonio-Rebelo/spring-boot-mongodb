2fb6e5e009b10bc928eb9728a2b76853ef8073b7
António Rebelo <tz.ajsr@gmail.com>
2024-01-09 10:57:46 +0000

    User by id

--- src/main/java/com/antoniorebelo/springmongo/services/UserService.java
+++ src/main/java/com/antoniorebelo/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.antoniorebelo.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.antoniorebelo.springmongo.domain.User;
 import com.antoniorebelo.springmongo.repository.UserRepository;
+import com.antoniorebelo.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
